00b703f275cd33008191322481fe530d20d32dd5
bajdcc <bajdcc@foxmail.com>
2015-10-27 12:31:26 +0800

    fixed border

--- NewsApp/MainWindow.xaml.cs
+++ NewsApp/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media.Animation;
 
 namespace NewsApp
@@ -12,35 +13,50 @@ namespace NewsApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool closed = false;
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Window_Closing(object sender, CancelEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!closed)
+            Dispatcher.Invoke(() =>
+                    BeginAnimation(UIElement.OpacityProperty,
+                    new DoubleAnimation(0, 0,
+                    new Duration(TimeSpan.FromSeconds(1)))));
+            Task.Factory.StartNew(() =>
             {
-                BeginAnimation(UIElement.OpacityProperty,
-                    new DoubleAnimation(1, 0,
-                    new Duration(TimeSpan.FromSeconds(2))));
-                e.Cancel = true;
-                Task.Factory.StartNew(()=>
-                {
-                    Thread.Sleep(TimeSpan.FromSeconds(2));
-                    Dispatcher.Invoke(() => Close());
-                });                
-                closed = true;
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+                Dispatcher.Invoke(() =>
+                    BeginAnimation(UIElement.OpacityProperty,
+                    new DoubleAnimation(0, 1,
+                    new Duration(TimeSpan.FromSeconds(2)))));
+            });
+        }
+
+        private void Window_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                AnimateClose();
             }
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AnimateClose();
+        }
+
+        private void AnimateClose()
         {
             BeginAnimation(UIElement.OpacityProperty,
-                new DoubleAnimation(0, 1,
+                new DoubleAnimation(1, 0,
                 new Duration(TimeSpan.FromSeconds(2))));
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Dispatcher.Invoke(() => Close());
+            });
         }
     }
 }
